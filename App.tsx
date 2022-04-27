bdc9a12e2e2cf5964f47c61fe01f424258d83697
Bruno Lamps <lamparolho@gmail.com>
2022-04-27 10:47:12 -0300

    ..

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-// import HomeScreen from './screens/HomeScreen';
-import PlannerScreen from './screens/PlannerScreen';
+import Navigation from './navigation';
 
 export default function App() {
   return (
-    <View>
-      <PlannerScreen></PlannerScreen>
+    <>
+      <Navigation></Navigation>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
