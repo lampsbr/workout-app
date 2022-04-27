6e943a0bb4c2a0130b4e2a8d2c81baae95015733
Bruno Lamps <lamparolho@gmail.com>
2022-04-27 15:17:28 -0300

    stack navigation introduced

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,16 @@
-import {View, Text} from 'react-native'
+import { useEffect } from 'react';
+import {View, Text, Button} from 'react-native'
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation}: any) {
+
+    useEffect(() => {
+        console.log('Rendering Home Screen')
+    }, []);
+
     return (
         <View>
             <Text>I am Home Screen</Text>
+            <Button title='go to Planner' onPress={() => navigation.push('Planner')}/>
         </View>
     )
 }
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,9 +1,16 @@
-import {View, Text} from 'react-native'
+import { useEffect } from 'react'
+import {View, Text, Button} from 'react-native'
 
-export default function PlannerScreen() {
+export default function PlannerScreen({navigation}: any) {
+    
+    useEffect(() => {
+        console.log('Rendering Planner Screen')
+    }, []);
+
     return (
         <View>
             <Text>I am PLANNER Screen</Text>
+            <Button title='go to Home' onPress={() => navigation.push('Home')}/>
         </View>
     )
 }
