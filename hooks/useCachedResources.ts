f78e93d66a8addf0916e39df308f8df3ddb142f9
Bruno Lamps <lamparolho@gmail.com>
2022-05-03 19:00:56 -0300

    early implementation of storage (using react-native-async-storage)

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import * as Font from 'expo-font'
+import { initWorkouts } from '../storage/workout';
+
 
 export default function useCachedResources() {
     const [isLoadingComplete, setIsLoadingComplete] = useState(false);
@@ -7,6 +9,8 @@ export default function useCachedResources() {
     useEffect(() => {
         async function loadResourcesAndDataAsync() {
             try {
+                //await clearWorkouts()
+                await initWorkouts()
                 await Font.loadAsync({
                     "montserrat": require ("../assets/fonts/Montserrat-Regular.ttf"),
                     "montserrat-bold": require ("../assets/fonts/Montserrat-Bold.ttf"),
